e48b118d841500f34a6624d83701e3ccf5fb2cdb
David4221 <david@lanworx.com.au>
2012-10-10 17:15:59 +1000

    Initial Commit

---
+++ Desktop/first_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '08e6ba7d138f91539dfe29cd87ca6e21bbba07885f8a5e6ecb10a8d59f5021f81021f1c0632120c4000a3687646488d7a17a2d0a6f7092f78a0b39a1f3f68206'
