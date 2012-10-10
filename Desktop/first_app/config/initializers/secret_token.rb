ccd3f32220412ab32284baffbfc65c456c32894f
David Rowe <david@lanworx.com.au>
2012-10-10 19:53:39 +1000

    Improved the README file

--- Desktop/first_app/config/initializers/secret_token.rb
+++ Desktop/first_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '08e6ba7d138f91539dfe29cd87ca6e21bbba07885f8a5e6ecb10a8d59f5021f81021f1c0632120c4000a3687646488d7a17a2d0a6f7092f78a0b39a1f3f68206'
+FirstApp::Application.config.secret_token = 'a74d85d90958b8152a24bcdd36aa11ac45be163c870fd7a7b1957575f647164805d5c887304dea22c74253271b0fd6010742f311e09890e67e41967e43deb136'
